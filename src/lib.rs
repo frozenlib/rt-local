82a30741f0e4d80d3e89484081356cc34895cf42
frozenlib <frozenlib@users.noreply.github.com>
2022-05-21 13:00:04 +0900

    Make `step()` public.

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ mod core;
 pub use crate::core::{spawn_local, Task};
 
 pub mod runtime {
-    pub use crate::core::{enter, leave, run, RuntimeBackend, RuntimeMainLoop, RuntimeWaker};
+    pub use crate::core::{enter, leave, run, step, RuntimeBackend, RuntimeMainLoop, RuntimeWaker};
 }
