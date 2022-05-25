ebffd6e4c8a26cfe46ec9daaf66ebfc4eaf56228
frozenlib <frozenlib@users.noreply.github.com>
2022-05-25 11:27:56 +0900

    Fix document.

--- rt-local/src/lib.rs
+++ rt-local/src/lib.rs
@@ -3,10 +3,10 @@ pub mod runtime {
     pub mod core {
 
         pub use rt_local_core::runtime::core::*;
-        pub use rt_local_macros::core_main as main;
-        pub use rt_local_macros::core_test as test;
+        pub use rt_local_macros::{core_main as main, core_test as test};
     }
 
     #[cfg(feature = "windows")]
+    #[doc(inline)]
     pub use rt_local_windows as windows;
 }
