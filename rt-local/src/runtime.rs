98af623a20f4d35e2fde02541b188b33b9d67ebc
frozenlib <frozenlib@users.noreply.github.com>
2023-08-20 19:12:28 +0900

    Add doc comment.

--- rt-local/src/runtime.rs
+++ rt-local/src/runtime.rs
@@ -2,7 +2,42 @@
 pub mod core {
 
     pub use rt_local_core::runtime::core::*;
-    pub use rt_local_macros::{core_main as main, core_test as test};
+
+    /// Mark the asynchronous function as a entry point.
+    ///
+    /// The asynchronous runtime is launched using [`run`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// #[rt_local::runtime::core::main]
+    /// async fn main() {
+    ///     // ...
+    /// }
+    /// ```
+    pub use rt_local_macros::core_main as main;
+
+    /// Mark the function as a test.
+    ///
+    /// When specified for an asynchronous function, use [`run`] to launch the asynchronous runtime.
+    /// When specified for a synchronous function, do not launch the asynchronous runtime.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rt_local::runtime::core::test;
+    ///
+    /// #[test]
+    /// async fn test_async() {
+    ///     // ...
+    /// }
+    ///
+    /// #[test]
+    /// fn test_sync() {
+    ///     // ..
+    /// }
+    /// ```
+    pub use rt_local_macros::core_test as test;
 }
 
 /// Runtime with Windows message loop.
