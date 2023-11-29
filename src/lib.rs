45e0a8013f4fbd04c10f54e5e18752027baa541e
frozenlib <frozenlib@users.noreply.github.com>
2023-11-29 13:09:28 +0900

    Change project layout.

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ pub use rt_local_core::{base, spawn_local, wait_for_idle, Task};
 pub mod runtime;
 
 #[cfg(doctest)]
-#[doc = include_str!("../../README.md")]
+#[doc = include_str!("../README.md")]
 pub mod test_readme {}
