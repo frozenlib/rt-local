10196020f767d786c05d62f29f82d9c38f789e64
frozenlib <frozenlib@users.noreply.github.com>
2023-11-29 13:22:38 +0900

    Split `rt-local-runtime-windows` crate.

--- runtime/windows/src/lib.rs
+++ runtime/windows/src/lib.rs
@@ -14,41 +14,6 @@ pub fn run<T>(future: impl Future<Output = T>) -> T {
     rt_local_core::base::run(&WindowsEventLoop::new(), future)
 }
 
-/// Mark the asynchronous function as a the entry point.
-///
-/// The asynchronous runtime is launched using [`run`].
-/// # Examples
-///
-/// ```
-/// #[rt_local::runtime::windows::main]
-/// async fn main() {
-///     // ...
-/// }
-/// ```
-pub use rt_local_macros::windows_main as main;
-
-/// Mark the function as a test.
-///
-/// When specified for an asynchronous function, use [`run`] to launch the asynchronous runtime.
-/// When specified for a synchronous function, do not launch the asynchronous runtime.
-///
-/// # Examples
-///
-/// ```
-/// use rt_local::runtime::windows::test;
-///
-/// #[test]
-/// async fn test_async() {
-///     // ...
-/// }
-///
-/// #[test]
-/// fn test_sync() {
-///     // ..
-/// }
-/// ```
-pub use rt_local_macros::windows_test as test;
-
 struct WindowsEventLoop {
     waker: Arc<Waker>,
     _not_send: PhantomData<*mut ()>,
--- src/runtime.rs
+++ src/runtime.rs
@@ -1,6 +1,5 @@
 /// Platform-independent runtime.
 pub mod blocking {
-
     pub use rt_local_core::runtime::blocking::*;
 
     /// Mark the asynchronous function as a entry point.
@@ -42,4 +41,41 @@ pub mod blocking {
 
 /// Runtime with Windows message loop.
 #[cfg(all(target_os = "windows", feature = "windows"))]
-pub mod windows;
+pub mod windows {
+    pub use rt_local_runtime_windows::*;
+
+    /// Mark the asynchronous function as a the entry point.
+    ///
+    /// The asynchronous runtime is launched using [`run`].
+    /// # Examples
+    ///
+    /// ```
+    /// #[rt_local::runtime::windows::main]
+    /// async fn main() {
+    ///     // ...
+    /// }
+    /// ```
+    pub use rt_local_macros::windows_main as main;
+
+    /// Mark the function as a test.
+    ///
+    /// When specified for an asynchronous function, use [`run`] to launch the asynchronous runtime.
+    /// When specified for a synchronous function, do not launch the asynchronous runtime.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rt_local::runtime::windows::test;
+    ///
+    /// #[test]
+    /// async fn test_async() {
+    ///     // ...
+    /// }
+    ///
+    /// #[test]
+    /// fn test_sync() {
+    ///     // ..
+    /// }
+    /// ```
+    pub use rt_local_macros::windows_test as test;
+}
