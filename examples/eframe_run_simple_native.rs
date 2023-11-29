67285fd608b06b2db8bee63d1389995f9c84bd9e
frozenlib <frozenlib@users.noreply.github.com>
2023-11-29 18:24:39 +0900

    Use tokio in example.

--- examples/eframe_run_simple_native.rs
+++ examples/eframe_run_simple_native.rs
@@ -4,33 +4,38 @@ use std::{
     time::Duration,
 };
 
-use async_std::task::sleep;
 use eframe::{NativeOptions, Result};
 use egui::{Label, ProgressBar};
 use rt_local::{runtime::eframe::run_simple_native, spawn_local};
+use tokio::time::sleep;
 
-fn main() -> Result<()> {
-    let options = NativeOptions::default();
-    let p = Rc::new(Cell::new(0.0));
-    let text = Rc::new(RefCell::new(String::new()));
-    let mut task = None;
-    run_simple_native("simple native", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            if ui.button("spawn").clicked() {
-                let p = p.clone();
-                let text = text.clone();
-                p.set(0.0);
-                task = Some(spawn_local(async move {
-                    for _ in 0..100 {
-                        p.set(p.get() + 0.01);
-                        *text.borrow_mut() = format!("{:.2}", p.get());
-                        sleep(Duration::from_millis(100)).await;
-                    }
-                    *text.borrow_mut() = "Done".to_string();
-                }));
-            }
-            ui.add(ProgressBar::new(p.get()));
-            ui.add(Label::new(text.borrow().as_str()));
-        });
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Using `block_in_place` is necessary, otherwise,
+    // if the budget is exhausted, Tokio's asynchronous functions will always return `Pending`, causing problems.
+    tokio::task::block_in_place(|| {
+        let options = NativeOptions::default();
+        let p = Rc::new(Cell::new(0.0));
+        let text = Rc::new(RefCell::new(String::new()));
+        let mut task = None;
+        run_simple_native("simple native", options, move |ctx, _frame| {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                if ui.button("spawn").clicked() {
+                    let p = p.clone();
+                    let text = text.clone();
+                    p.set(0.0);
+                    task = Some(spawn_local(async move {
+                        for _ in 0..200 {
+                            p.set(p.get() + 0.005);
+                            *text.borrow_mut() = format!("{:.2}", p.get());
+                            sleep(Duration::from_millis(10)).await;
+                        }
+                        *text.borrow_mut() = "Done".to_string();
+                    }));
+                }
+                ui.add(ProgressBar::new(p.get()));
+                ui.add(Label::new(text.borrow().as_str()));
+            });
+        })
     })
 }
