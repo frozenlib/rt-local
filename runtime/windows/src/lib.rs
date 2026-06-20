01bcbd6b6389408db5755af4398d03133a48a88b
frozenlib <frozenlib@users.noreply.github.com>
2026-06-21 00:27:27 +0900

    Add TSF support.

--- runtime/windows/src/lib.rs
+++ runtime/windows/src/lib.rs
@@ -1,34 +1,134 @@
 #![cfg(target_os = "windows")]
 
 use rt_local_core::base::{idle, EventLoop};
-use std::{future::Future, marker::PhantomData, ops::ControlFlow, sync::Arc, task::Wake};
-use windows::Win32::{
-    Foundation::{LPARAM, WPARAM},
-    System::Threading::GetCurrentThreadId,
-    UI::WindowsAndMessaging::{
-        DispatchMessageW, GetMessageW, PeekMessageW, PostThreadMessageW, TranslateMessage, MSG,
-        PM_REMOVE, WM_NULL, WM_QUIT,
+use std::{
+    cell::Cell, future::Future, marker::PhantomData, ops::ControlFlow, sync::Arc, task::Wake,
+};
+use windows::{
+    core::Interface,
+    Win32::{
+        Foundation::{HWND, LPARAM, WPARAM},
+        System::Threading::GetCurrentThreadId,
+        UI::{
+            TextServices::{ITfKeystrokeMgr, ITfMessagePump, ITfThreadMgr},
+            WindowsAndMessaging::{
+                DispatchMessageW, GetMessageW, PeekMessageW, PostThreadMessageW, TranslateMessage,
+                MSG, PM_REMOVE, WM_KEYDOWN, WM_KEYUP, WM_NULL, WM_QUIT, WM_SYSKEYDOWN, WM_SYSKEYUP,
+            },
+        },
     },
 };
 
+thread_local! {
+    static USE_TSF_KEYSTROKE_MGR: Cell<bool> = const { Cell::new(true) };
+}
+
+/// Options for [`run_with_options`].
+#[derive(Clone, Default)]
+pub struct RunOptions {
+    /// Thread manager used for Text Services Framework integration.
+    ///
+    /// The caller is responsible for COM initialization and for activating and deactivating the
+    /// thread manager.
+    pub tsf_thread_mgr: Option<ITfThreadMgr>,
+}
+
 /// Executes the specified future and blocks until it completes.
 pub fn run<T>(future: impl Future<Output = T>) -> T {
-    rt_local_core::base::run(&WindowsEventLoop::new(), future)
+    run_with_options(future, &RunOptions::default())
+}
+
+/// Executes the specified future with options and blocks until it completes.
+pub fn run_with_options<T>(future: impl Future<Output = T>, options: &RunOptions) -> T {
+    rt_local_core::base::run(&WindowsEventLoop::new(options), future)
+}
+
+/// Sets whether [`ITfKeystrokeMgr`] is used on this thread.
+pub fn set_use_tsf_keystroke_mgr(value: bool) {
+    USE_TSF_KEYSTROKE_MGR.set(value);
 }
 
 struct WindowsEventLoop {
     waker: Arc<Waker>,
+    tsf: Option<Tsf>,
     _not_send: PhantomData<*mut ()>,
 }
 
 impl WindowsEventLoop {
-    fn new() -> Self {
+    fn new(options: &RunOptions) -> Self {
         unsafe {
             let thread_id = GetCurrentThreadId();
             Self {
                 waker: Arc::new(Waker { thread_id }),
+                tsf: options
+                    .tsf_thread_mgr
+                    .as_ref()
+                    .map(Tsf::new)
+                    .transpose()
+                    .unwrap(),
                 _not_send: PhantomData,
             }
+        }
+    }
+
+    unsafe fn peek_message(&self, msg: &mut MSG) -> bool {
+        if let Some(tsf) = &self.tsf {
+            let mut result = false.into();
+            tsf.message_pump
+                .PeekMessageW(msg, HWND::default(), 0, 0, PM_REMOVE.0, &mut result)
+                .unwrap();
+            result.as_bool()
+        } else {
+            PeekMessageW(msg, None, 0, 0, PM_REMOVE).as_bool()
+        }
+    }
+
+    unsafe fn get_message(&self, msg: &mut MSG) {
+        if let Some(tsf) = &self.tsf {
+            let mut result = false.into();
+            tsf.message_pump
+                .GetMessageW(msg, HWND::default(), 0, 0, &mut result)
+                .unwrap();
+        } else {
+            GetMessageW(msg, None, 0, 0).ok().unwrap();
+        }
+    }
+
+    unsafe fn handle_tsf_keystroke(&self, msg: &MSG) -> bool {
+        if !USE_TSF_KEYSTROKE_MGR.get() {
+            return false;
+        }
+
+        let Some(tsf) = &self.tsf else {
+            return false;
+        };
+
+        match msg.message {
+            WM_KEYDOWN | WM_SYSKEYDOWN
+                if tsf
+                    .keystroke_mgr
+                    .TestKeyDown(msg.wParam, msg.lParam)
+                    .unwrap()
+                    .as_bool() =>
+            {
+                tsf.keystroke_mgr
+                    .KeyDown(msg.wParam, msg.lParam)
+                    .unwrap()
+                    .as_bool()
+            }
+            WM_KEYUP | WM_SYSKEYUP
+                if tsf
+                    .keystroke_mgr
+                    .TestKeyUp(msg.wParam, msg.lParam)
+                    .unwrap()
+                    .as_bool() =>
+            {
+                tsf.keystroke_mgr
+                    .KeyUp(msg.wParam, msg.lParam)
+                    .unwrap()
+                    .as_bool()
+            }
+            _ => false,
         }
     }
 }
@@ -43,20 +143,35 @@ impl EventLoop for WindowsEventLoop {
             }
             let mut msg = MSG::default();
             unsafe {
-                if !PeekMessageW(&mut msg, None, 0, 0, PM_REMOVE).as_bool() {
+                if !self.peek_message(&mut msg) {
                     if idle() {
                         continue;
                     } else {
-                        GetMessageW(&mut msg, None, 0, 0).ok().unwrap();
+                        self.get_message(&mut msg);
                     }
                 }
                 if msg.message == WM_QUIT {
                     panic!("message loop terminated");
                 }
-                let _ = TranslateMessage(&msg);
-                DispatchMessageW(&msg);
+                if !self.handle_tsf_keystroke(&msg) {
+                    let _ = TranslateMessage(&msg);
+                    DispatchMessageW(&msg);
+                }
             }
         }
+    }
+}
+
+struct Tsf {
+    message_pump: ITfMessagePump,
+    keystroke_mgr: ITfKeystrokeMgr,
+}
+impl Tsf {
+    fn new(tsf_thread_mgr: &ITfThreadMgr) -> windows::core::Result<Self> {
+        Ok(Self {
+            message_pump: tsf_thread_mgr.cast()?,
+            keystroke_mgr: tsf_thread_mgr.cast()?,
+        })
     }
 }
 
--- tests/windows.rs
+++ tests/windows.rs
@@ -1,5 +1,28 @@
 #![cfg(all(target_os = "windows", feature = "windows"))]
-use rt_local::runtime::windows::run;
+use rt_local::runtime::windows::{run, run_with_options, set_use_tsf_keystroke_mgr, RunOptions};
+
+#[test]
+fn run_with_options_without_tsf() {
+    let mut executed = false;
+    run_with_options(
+        async {
+            executed = true;
+        },
+        &RunOptions::default(),
+    );
+    assert!(executed);
+}
+
+#[test]
+fn run_without_tsf_keystroke_mgr() {
+    set_use_tsf_keystroke_mgr(false);
+    let mut executed = false;
+    run(async {
+        executed = true;
+    });
+    set_use_tsf_keystroke_mgr(true);
+    assert!(executed);
+}
 
 mod test_utils;
 mod common {
